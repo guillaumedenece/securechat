0d79e243ef844abff462a635b5cff35f3deec4f7
guillaumedenece <guillaume.denece@gmail.com>
2017-11-14 15:56:17 -0800

    add jwt decode to /receive route

--- api/messages/receive/index.js
+++ api/messages/receive/index.js
@@ -2,13 +2,21 @@
 
 const express = require('express');
 const receiveController = require('./receive.controller');
+const jwt = require('../../../services/jwt');
 
 const router = express.Router({
     mergeParams: true
 });
 
 router.post('/', (req, res) => {
-  res.send({'success':'true'});
+  jwt.verify(req.headers.authorize)
+  .then( (decode) => {
+    res.send({'decode':decode});
+  })
+  .catch( (error) => {
+    console.log(error);
+    res.send({error: 'an error has occured'});
+  })
 });
 
 module.exports = router;
