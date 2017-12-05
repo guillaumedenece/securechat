22f4ff8c6e2e16c7f855b9c2af54d3ea27956221
Guillaume Dénecé <guillaumedenece@MacBook-Pro-de-Guillaume.local>
2017-12-04 23:39:45 -0800

    fix ssl config

--- config/express.js
+++ config/express.js
@@ -2,7 +2,7 @@ module.exports = {
   port: 3000,
   useSSL: false,
   ssl: {
-    privateKey: '/etc/ssl/private/ssl-cert-snakeoil.key',
-    certificate: '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+    privateKey: '/etc/letsencrypt/live/thesecurechat.me/privkey.pem',
+    certificate: '/etc/letsencrypt/live/thesecurechat.me/fullchain.pem'
   }
 }
