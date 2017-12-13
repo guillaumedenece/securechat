a80a1ab13b8656d8b21cc46a8e57d277238a7a5f
Guillaume Dénecé <guillaumedenece@dhcp-39-142-219.dhcpint.csulb.edu>
2017-12-12 21:26:51 -0800

    configuration varaibles take are configurable through environment variables

--- config/express.js
+++ config/express.js
@@ -2,7 +2,7 @@ module.exports = {
   port: 3000,
   useSSL: false,
   ssl: {
-    privateKey: '/etc/letsencrypt/live/thesecurechat.me/privkey.pem',
-    certificate: '/etc/letsencrypt/live/thesecurechat.me/fullchain.pem'
+    privateKey: process.env.PRIVATE_KEY_PATH || '/etc/letsencrypt/live/thesecurechat.me/privkey.pem',
+    certificate: process.env.CERTIFICATE_PATH || '/etc/letsencrypt/live/thesecurechat.me/fullchain.pem'
   }
 }
--- config/general.js
+++ config/general.js
@@ -1,5 +1,5 @@
 module.exports = {
-  challenge_validity_time: 1 * 60 * 1000,
-  token_validity_time: '1h',
-  token_secret_key: 'ireynueyctnrepcuzeaurcyrebeagzgtrgrtrhzr998t'
+  challenge_validity_time: process.env.CHALLENGE_VALIDITY_TIME || 1 * 60 * 1000,
+  token_validity_time: process.env.TOKEN_VALIDITY_TIME || '1h',
+  token_secret_key: process.env.TOKEN_SECRET_KEY || 'secret_key'
 };
